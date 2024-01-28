b22c045ad786a4dc1411b1a295f7375e7085bd18
Victor <victormarquesrina@hotmail.com>
2024-01-28 13:14:56 +0000

    Add DOM elements and event listeners

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,17 @@
 // API key 
 var myApiKey = "57343ed715e45df35cda2f1c919f719d";
+
+// DOM Elements
+var searchForm = document.querySelector("#city-search-form");
+var searchInput = document.querySelector("#city-input");
+var searchButton = document.querySelector("#city-search-button");
+var currentWeatherContainer = document.querySelector("#current-weather");
+var weatherForecastContainer = document.querySelector("#weather-forecast");
+var searchHistoryContainer = document.querySelector("#search-history");
+
+// Search history array
+var searchHistory = [];
+
+// Event listeners
+searchForm.addEventListener("submit", handleSearchFormSubmit);
+searchButton.addEventListener("click", handleSearchFormSubmit);
