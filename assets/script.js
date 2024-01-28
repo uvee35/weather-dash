f54a1795c1f62ec598f672c282a3dc0126687381
Victor <victormarquesrina@hotmail.com>
2024-01-28 13:48:28 +0000

    Refactor DOM element IDs and event listeners fixed

--- assets/script.js
+++ assets/script.js
@@ -2,16 +2,9 @@
 var myApiKey = "57343ed715e45df35cda2f1c919f719d";
 
 // DOM Elements
-var searchForm = document.querySelector("#city-search-form");
-var searchInput = document.querySelector("#city-input");
-var searchButton = document.querySelector("#city-search-button");
-var currentWeatherContainer = document.querySelector("#current-weather");
-var weatherForecastContainer = document.querySelector("#weather-forecast");
-var searchHistoryContainer = document.querySelector("#search-history");
-
-// Search history array
-var searchHistory = [];
-
-// Event listeners
-searchForm.addEventListener("submit", handleSearchFormSubmit);
-searchButton.addEventListener("click", handleSearchFormSubmit);
+var searchForm = document.querySelector("#search-form");
+var searchInput = document.querySelector("#search-input");
+var searchButton = document.querySelector("#search-button");
+var currentWeatherContainer = document.querySelector("#today");
+var weatherForecastContainer = document.querySelector("#forecast");
+var searchHistoryContainer = document.querySelector("#history");
